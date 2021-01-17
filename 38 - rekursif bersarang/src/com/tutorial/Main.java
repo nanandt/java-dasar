b3f53ec53874f2bb2eefcd3df0f995fb592bdd9f
nanandt <rizkyfatih007@gmail.com>
2021-01-18 00:14:36 +0700

    rekursif bercabang

--- 38 - rekursif bersarang/src/com/tutorial/Main.java
+++ 38 - rekursif bersarang/src/com/tutorial/Main.java
@@ -1,4 +1,24 @@
 package com.tutorial;
+import java.lang.String;
+import java.util.Scanner;
 
 public class Main {
+  public static void main(String[] args) {
+
+    Scanner userInput = new Scanner(System.in);
+    System.out.print("masukan nilai n = ");
+    int nilai = userInput.nextInt();
+    int nilai_fibonacci = fibonacci(nilai, "atas");
+    System.out.println("nilai fibonacci ke-"+ nilai+" adalah " + nilai_fibonacci);
+  }
+
+  private static int fibonacci(int n, String daun){
+    System.out.println("daun" + daun);
+    System.out.println("fibonacci ke -n " +n);
+    if(n == 1 || n == 0){
+      return n;
+    } else{
+      return fibonacci(n-1, "kiri") + fibonacci(n-2, "kanan");
+    }
+  }
 }
